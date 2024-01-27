8cddeea99e4d73326ef91e9940059cf00172bf2f
sushovan7 <bhattaraisushovan999@gmail.com>
2024-01-27 21:50:31 +0545

    hamburger menu updated

--- file.js
+++ file.js
@@ -1,49 +1,56 @@
-ScrollReveal().reveal('.nav',{
-  origin:'bottom',
-  distance:'20px',
-  duration:1000,
-  opacity:0,
-  delay:500,
-})
+scrollAnimations()
+hamburgerDisplay()
 
-ScrollReveal().reveal('.load',{
-  origin:'bottom',
-  distance:'50px',
-  duration:2000,
-  opacity:0,
- 
-})
+function scrollAnimations(){
+  ScrollReveal().reveal('.nav',{
+    origin:'bottom',
+    distance:'20px',
+    duration:1000,
+    opacity:0,
+    delay:500,
+  })
+  
+  ScrollReveal().reveal('.load',{
+    origin:'bottom',
+    distance:'50px',
+    duration:2000,
+    opacity:0,
+   
+  })
+  
+  ScrollReveal().reveal('.title',{
+    origin:'bottom',
+    distance:'20px',
+    duration:1000,
+    opacity:0,
+    delay:1000,
+  })
+  
+  ScrollReveal().reveal('.section',{
+    origin:'bottom',
+    distance:'20px',
+    opacity:0,
+    duration:1300,
+    delay:100,
+  })
+  
+}
 
-ScrollReveal().reveal('.title',{
-  origin:'bottom',
-  distance:'20px',
-  duration:1000,
-  opacity:0,
-  delay:1000,
-})
+function hamburgerDisplay(){
+  let hamBurgerMenu = document.querySelector('.ham-burger');
+let navLink = document.querySelector('.nav-links');
+let crossBtn = document.querySelector('.cross-btn');
 
-ScrollReveal().reveal('.section',{
-  origin:'bottom',
-  distance:'20px',
-  opacity:0,
-  duration:1300,
-  delay:100,
-})
+hamBurgerMenu.addEventListener('click',displayHamburgerMenu)
+function displayHamburgerMenu(){
+  navLink.style.display = 'block'
+}
+
+crossBtn.addEventListener('click',removeHamBurgerMenu)
+function removeHamBurgerMenu(){
+navLink.style.display = 'none'
+}
+}
 
 
-// let hamBurgerMenu = document.querySelector('.hamburger');
-// let navLink = document.querySelector('.nav-links');
-// let crossBtn = document.querySelector('.cross-btn');
 
-// hamBurgerMenu.addEventListener('click',displayHamburgerMenu)
-// function displayHamburgerMenu(){
-//   navLink.style.display = 'block'
-//   navLink.style.backgroundColor ='red'
-// }
-
-// crossBtn.addEventListener('click',removeHamburgerMenu)
-// function removeHamburgerMenu(){
-//   console.log('heloo')
-//   navLink.style.display='none';
-//   navLink.style.backgroundColor ='black'
-// }
