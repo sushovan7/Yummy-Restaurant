a4b05e71960ba0c8c3cfb95476bda9ff43edf6b1
sushovan7 <bhattaraisushovan999@gmail.com>
2024-01-27 18:32:05 +0545

    hamburger menu update

--- file.js
+++ file.js
@@ -29,3 +29,21 @@ ScrollReveal().reveal('.section',{
   duration:1300,
   delay:100,
 })
+
+
+// let hamBurgerMenu = document.querySelector('.hamburger');
+// let navLink = document.querySelector('.nav-links');
+// let crossBtn = document.querySelector('.cross-btn');
+
+// hamBurgerMenu.addEventListener('click',displayHamburgerMenu)
+// function displayHamburgerMenu(){
+//   navLink.style.display = 'block'
+//   navLink.style.backgroundColor ='red'
+// }
+
+// crossBtn.addEventListener('click',removeHamburgerMenu)
+// function removeHamburgerMenu(){
+//   console.log('heloo')
+//   navLink.style.display='none';
+//   navLink.style.backgroundColor ='black'
+// }
