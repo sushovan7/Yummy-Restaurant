03ac75558bcef1e12f39c639d6b098dbfc8d5681
sushovan7 <bhattaraisushovan999@gmail.com>
2024-01-27 17:21:28 +0545

    first commit

---
+++ file.js
@@ -0,0 +1,31 @@
+ScrollReveal().reveal('.nav',{
+  origin:'bottom',
+  distance:'20px',
+  duration:1000,
+  opacity:0,
+  delay:500,
+})
+
+ScrollReveal().reveal('.load',{
+  origin:'bottom',
+  distance:'50px',
+  duration:2000,
+  opacity:0,
+ 
+})
+
+ScrollReveal().reveal('.title',{
+  origin:'bottom',
+  distance:'20px',
+  duration:1000,
+  opacity:0,
+  delay:1000,
+})
+
+ScrollReveal().reveal('.section',{
+  origin:'bottom',
+  distance:'20px',
+  opacity:0,
+  duration:1300,
+  delay:100,
+})
